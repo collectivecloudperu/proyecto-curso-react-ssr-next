19e475626117d249b902313a1c0f521e25ddb355
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2026-03-18 21:10:45 -0500

    Add files via upload

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -1,9 +1,11 @@
 import NosotrosHero from "../components/NosotrosHero";
+import Historia from "../components/Historia";
 
 export default function NosotrosPage() {
     return (
         <main>
             <NosotrosHero />
+            <Historia />
         </main>
     )
 }
